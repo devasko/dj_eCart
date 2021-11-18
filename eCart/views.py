1f1fb302978f4594a9836fb77b9818a7e5e68be4
Alex Korobov <devasko@ro.ru>
2021-11-18 13:14:10 +0300

    Static files added

--- eCart/views.py
+++ eCart/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>HomePage</h1>')
+    return render(request, 'home.html')
