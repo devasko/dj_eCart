18da6de9d3c3389399eb31ef83fcfd92e8de9d17
Alex Korobov <devasko@ro.ru>
2021-11-22 18:38:32 +0300

    product detail view + page dynamic

--- store/views.py
+++ store/views.py
@@ -22,4 +22,12 @@ def store(request, category_slug=None):
 
 
 def detail(request, category_slug, product_slug):
-    return render(request, 'store/detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {'single_product': single_product,
+              }
+
+    return render(request, 'store/detail.html', context)
