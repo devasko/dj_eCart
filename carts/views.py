572bc29f8b0f8b717e749700e3d885e520ff4ed2
Alex Korobov <devasko@ro.ru>
2021-12-04 11:57:43 +0300

    Cart page data added

--- carts/views.py
+++ carts/views.py
@@ -27,8 +27,6 @@ def add_cart(request, product_id):
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
         cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()
 
     return redirect('cart')
 
